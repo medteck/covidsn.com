bbdd7f1be5123417df30d7dfca903eb97fc1b3f9
Medteck <medteck15@gmail.com>
2020-03-15 15:33:26 -0400

    Logging host

--- pages/index.vue
+++ pages/index.vue
@@ -19,6 +19,7 @@ export default {
     if (process.server) {
       host = `http://${req.headers.host}`
     }
+    console.log(`Host: ${host}`)
 
     const accueil = await $axios.get(`${host}/api/pages/accueil`)
     const communications = await $axios.get(`${host}/api/collections/communications`)
