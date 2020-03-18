9a1caf20fef90ea72fa6a7d45ca05dd9ff9e6614
Medteck <medteck15@gmail.com>
2020-03-17 21:56:41 -0400

    Replace dashboard link for the new one

--- components/partials/PowerBiDashboard.vue
+++ components/partials/PowerBiDashboard.vue
@@ -3,7 +3,7 @@
     <iframe
       id="power-bi-dashboard"
       class="w-full"
-      src="https://app.powerbi.com/view?r=eyJrIjoiNGQzZTYxM2ItNTMyMC00MDYwLTgxNjMtZDE2Y2U2YWQxMmFjIiwidCI6ImE5ZTFjNmI2LTFlZDQtNDY5ZC1hZTBmLTA5MWQ3ZTRhZWQ1ZCIsImMiOjl9"
+      src="https://app.powerbi.com/view?r=eyJrIjoiNWYzOTczMTMtODk5YS00MzRlLWExYjEtM2VhMDM4MWQ2OTE2IiwidCI6ImE5ZTFjNmI2LTFlZDQtNDY5ZC1hZTBmLTA5MWQ3ZTRhZWQ1ZCIsImMiOjl9"
       frameborder="0"
     />
   </div>
