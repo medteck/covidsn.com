fe2195cbe88519dd1d52ffc10dbe2d002cbb5f77
Medteck <medteck15@gmail.com>
2020-03-21 08:19:00 -0400

    Dashboard on mobile screens

--- components/partials/PowerBiDashboard.vue
+++ components/partials/PowerBiDashboard.vue
@@ -1,16 +1,55 @@
 <template>
   <div class="w-full items-stretch">
     <iframe
-      id="power-bi-dashboard"
-      class="w-full"
+      v-if="!isMobile"
+      id="power-bi-dashboard-desktop"
+      class="w-full hidden md:block"
       src="https://app.powerbi.com/view?r=eyJrIjoiNWYzOTczMTMtODk5YS00MzRlLWExYjEtM2VhMDM4MWQ2OTE2IiwidCI6ImE5ZTFjNmI2LTFlZDQtNDY5ZC1hZTBmLTA5MWQ3ZTRhZWQ1ZCIsImMiOjl9"
+      frameborder="0"
+    />
+    <iframe
+      v-if="isMobile"
+      id="power-bi-dashboard-mobile"
+      class="w-full md:hidden"
+      src="https://app.powerbi.com/view?r=eyJrIjoiNDQyNDhmY2ItODE3OC00ODJlLTgzNzYtNDA0MzMyOTZkZGRhIiwidCI6ImE5ZTFjNmI2LTFlZDQtNDY5ZC1hZTBmLTA5MWQ3ZTRhZWQ1ZCIsImMiOjl9"
       frameborder="0"
     />
   </div>
 </template>
 
+<script>
+export default {
+  data () {
+    return {
+      windowWidth: document.documentElement.clientWidth
+    }
+  },
+  computed: {
+    isMobile () {
+      return this.windowWidth <= 768
+    }
+  },
+  methods: {
+    getWindowWidth () {
+      this.windowWidth = document.documentElement.clientWidth
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this.getWindowWidth)
+    // Init
+    this.getWindowWidth()
+  },
+  beforeDestroy () {
+    window.removeEventListener('resize', this.getWindowWidth)
+  }
+}
+</script>
+
 <style scoped>
-  #power-bi-dashboard {
+  #power-bi-dashboard-desktop {
     height: calc(60vw - 80px);
+  }
+  #power-bi-dashboard-mobile {
+    height: calc(250vw - 80px);
   }
 </style>
--- components/sections/Donnees.vue
+++ components/sections/Donnees.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="section-wrapper p-4">
-    <div class="my-6 px-4">
-      <h1 class="text-left font-bold md:font-light">
-        Données statiques du COVID-19 sur le territoire
-      </h1>
-      <p class="mt-4 text-gray-600 text-lg">
-        Ces données ont été mises à jour pour à la date suivante : <b class="text-black">{{ donnees.modified_on }}</b>
-      </p>
-    </div>
-    <client-only placeholder="Chargement...">
-      <div class="w-full p-6 mt-10 md:hidden">
-        <DataBlocks :donnees="donnees" :show-text="true" :show-updated="true"  />
-      </div>
-      <div v-if="!isMobile" class="w-full p-6 hidden md:block">
+    <client-only placeholder="Chargement des données...">
+      <div class="w-full md:p-6">
         <PowerBiDashboard />
       </div>
     </client-only>
@@ -20,27 +9,16 @@
 </template>
 
 <script>
-import DataBlocks from '~/components/partials/DataBlocks.vue'
 import PowerBiDashboard from '~/components/partials/PowerBiDashboard.vue'
 
 export default {
   components: {
-    DataBlocks,
     PowerBiDashboard
   },
   props: {
     donnees: {
       required: true,
       type: Object
-    }
-  },
-  computed: {
-    isMobile () {
-      if (process.client) {
-        return window.innerWidth <= 768
-      } else {
-        return true
-      }
     }
   },
   head () {
